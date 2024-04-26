c116e18cbcc1d152fa132ee8b9f3dfeff1f6371a
efficacy38 <efficacy38@gmail.com>
2024-04-27 03:12:30 +0800

    fix: shell script error

--- tailscale-derp/entrypoint.sh
+++ tailscale-derp/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -ue
+set -eu
 
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
-tailscaled --tun=userspace-networking &
+tailscaled --tun=userspace-networking "${TAILSCALED_STATE_ARG}" &
 PID=$!
 
 # connect to tailscale
@@ -13,10 +13,10 @@ until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOS
 done
 
 echo "Starting Tailscale DERP server"
-derper -hostname=$DERP_DOMAIN \
-    -certmode=$DERP_CERT_MODE \
-    -certdir=$DERP_CERT_DIR \
-    -verify-clients=$DERP_VERIFY_CLIENTS
+derper -hostname="${DERP_DOMAIN}" \
+    -certmode="${DERP_CERT_MODE}" \
+    -certdir="${DERP_CERT_DIR}" \
+    -verify-clients="${DERP_VERIFY_CLIENTS}"
 
 # exit if tailscaled exit
 wait ${PID}
